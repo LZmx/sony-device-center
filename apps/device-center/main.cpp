d2737b5bba27908e166a26311336f76e6c9f2957
Angel Celio <LZmx@users.noreply.github.com>
2026-09-11 16:50:12 +0800

    fix(macos): use Qt QML deployment API

--- apps/device-center/main.cpp
+++ apps/device-center/main.cpp
@@ -24,15 +24,10 @@ int main(int argc, char *argv[]) {
 
     QQmlApplicationEngine engine;
     engine.rootContext()->setContextProperty("controller", &controller);
+    engine.loadFromModule("SonyDeviceCenter", "Main");
 
-    const QUrl url(QStringLiteral("qrc:/qml/Main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-
-    engine.load(url);
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
 }
